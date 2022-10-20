a3fafdee328584cf9340d7a70fd6514cbb990cfb
CatMiaogu <1016489713@qq.com>
2022-10-20 21:00:52 +0800

    update

--- docs/blog/shuyu/shuyu.md
+++ docs/blog/shuyu/shuyu.md
@@ -24,3 +24,7 @@ Pinia 是类似Vuex的存储库，它允许您跨组件/页面共享状态。与
 Weex 致力于使开发者能基于通用跨平台的 Web 开发语言和开发经验，来构建 Android、iOS 和 Web 应用。简单来说，在集成了 WeexSDK 之后，你可以使用 JavaScript 语言和前端开发经验来开发移动应用。
 
 Weex 渲染引擎与 DSL 语法层是分开的，Weex 并不强依赖任何特定的前端框架。目前 [Vue.js](https://vuejs.org/guide/introduction.html) 和 [Rax](https://rax.js.org/docs/guide/about)这两个前端框架被广泛应用于 Weex 页面开发，同时 Weex 也对这两个前端框架提供了最完善的支持。Weex 的另一个主要目标是跟进流行的 Web 开发技术并将其和原生开发的技术结合，实现开发效率和运行性能的高度统一。在开发阶段，一个 Weex 页面就像开发普通网页一样；在运行时，Weex 页面又充分利用了各种操作系统的原生组件和能力
+
+
+### [Express](https://www.expressjs.com.cn/)
+> 基于 Node.js 平台，快速、开放、极简的 Web 开发框架
--- docs/blog/test.js
+++ docs/blog/test.js
@@ -1,36 +1,19 @@
-// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// function shuffle(array) {
-//   let len = array.length;
-//   while (len) {
-//     let index = Math.floor(Math.random() * len--);
-//     [array[index], array[len]] = [array[len], array[index]];
-//   }
-//   return array;
-// }
-// console.log(shuffle(arr));
-
-// let b = [3, 2, 5, 7, 4, 8, 1, 6, 0, 9];
-
-// 防抖
-function debounce(fn, wait) {
-  let timer = null;
+function debounce(func, wait, immediate) {
+  let timeout;
   return function () {
-    if (timer !== null) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(fn, wait);
-  };
-}
-// 节流
-function throttle(fn, wait) {
-  let prev = Date.now();
-  return function () {
-    let contxt = this;
-    let args = arguments;
-    var now = Date.now();
-    if (now - prev > wait) {
-      fn.apply(contxt, args);
-      prev = Date.now();
+    const context = this;
+    const args = arguments;
+    if (timeout) clearTimeout(timeout);
+    if (immediate) {
+      const callNow = !timeout;
+      timeout = setTimeout(function () {
+        timeout = null;
+      }, wait);
+      if (callNow) func.apply(context, args);
+    } else {
+      timeout = setTimeout(function () {
+        func.apply(context, args);
+      }, wait);
     }
   };
 }
